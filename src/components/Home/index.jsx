eaaf4635ad4edde8d823990cff57d0f989bd8648
vineet <jammackp@gmail.com>
2025-05-19 22:38:10 +0530

    update 2

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import "./index.css";
+// import reactImg from '../../assets/coder.svg';
 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to My Portfolio</h1>
-      <p>This is the home page of my portfolio website.</p>
+    <div className="home-page">
+      <div className="hero-cont">
+          <ul class="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        <h1>Hello I am Vineet</h1>
+        <p>Currently Working as Software Engineer at Samsung.</p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home;
